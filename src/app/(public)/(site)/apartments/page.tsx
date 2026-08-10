8daa564569fd75da70c2572ab385f97eb695561d
mldjordje <mldjordje@users.noreply.github.com>
2026-08-10 04:51:08 +0200

    feat: redesign apartment browsing and booking

--- src/app/(public)/(site)/apartments/page.tsx
+++ src/app/(public)/(site)/apartments/page.tsx
@@ -1,5 +1,5 @@
-import ApartmentCard from '@/components/site/ApartmentCard';
-import PageHero from '@/components/site/PageHero';
+import { EditorialApartmentIndex } from '@/components/site/cinematic/EditorialApartmentIndex';
+import EditorialPageIntro from '@/components/site/shared/EditorialPageIntro';
 import { readStayData } from '@/lib/stay/store';
 
 export default async function ApartmentsPage() {
@@ -7,22 +7,12 @@ export default async function ApartmentsPage() {
 
 	return (
 		<>
-			<PageHero
-				kicker="Kolekcija apartmana"
-				title="Izaberi smestaj za city break, posao ili porodicni dolazak u Nis."
-				description="Svaki apartman ima sopstveni kalendar, direktan booking upit i pripremljen Booking.com export link."
+			<EditorialPageIntro
+				kicker="Apartmani u Nišu"
+				title="Izaberite svoj ritam grada."
+				description="Tri pažljivo uređena prostora za miran san, sporije jutro i grad koji vam je nadohvat koraka."
 			/>
-			<section className="tw-pb-18">
-				<div className="container">
-					<div className="row g-4">
-						{data.apartments.map((apartment) => (
-							<div key={apartment.id} className="col-lg-6">
-								<ApartmentCard apartment={apartment} />
-							</div>
-						))}
-					</div>
-				</div>
-			</section>
+			<EditorialApartmentIndex apartments={data.apartments} />
 		</>
 	);
 }
